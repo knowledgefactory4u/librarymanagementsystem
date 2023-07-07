2a9874530ff4b0be82d1dd69255a42ecb7723cad
Navdeep Kaur <kaurnav4199@gmail.com>
2023-07-07 17:25:25 -0400

    Update ApplicationTests.java

--- src/test/java/com/quickstartdev/librarymanagementsystem/ApplicationTests.java
+++ src/test/java/com/quickstartdev/librarymanagementsystem/ApplicationTests.java
@@ -3,7 +3,7 @@ package com.quickstartdev.librarymanagementsystem;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes= Application.class)
 class ApplicationTests {
 
 	@Test
