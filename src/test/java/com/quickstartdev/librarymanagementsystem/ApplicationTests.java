f811e140a68e0c3ebb4c6c94d282aab2d5c0d53a
Navdeep Kaur <kaurnav4199@gmail.com>
2023-07-25 11:36:59 -0400

    #12 fixed unit test

--- src/test/java/com/quickstartdev/librarymanagementsystem/ApplicationTests.java
+++ src/test/java/com/quickstartdev/librarymanagementsystem/ApplicationTests.java
@@ -1,13 +1,18 @@
 package com.quickstartdev.librarymanagementsystem;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import com.knf.dev.librarymanagementsystem.Application;
 @SpringBootTest(classes=Application.class)
 class ApplicationTests {
 
+	@Autowired
+	Application app;
 	@Test
 	void contextLoads() {
+		Assertions.assertNotNull(app, "Application context is not loaded successfully.");
 	}
 
 }
