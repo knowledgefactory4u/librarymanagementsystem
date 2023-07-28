dfba638e7cef5a9e7dbf043aff29d1c82e193267
Navdeep Kaur <kaurnav4199@gmail.com>
2023-07-28 14:48:30 -0400

    fix security hotspot

--- src/main/java/com/knf/dev/librarymanagementsystem/controller/CategoryController.java
+++ src/main/java/com/knf/dev/librarymanagementsystem/controller/CategoryController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.knf.dev.librarymanagementsystem.entity.Category;
@@ -21,14 +22,14 @@ public class CategoryController {
 
 	}
 
-	@RequestMapping("/categories")
+	@GetMapping("/categories")
 	public String findAllCategories(Model model) {
 
 		model.addAttribute("categories", categoryService.findAllCategories());
 		return "list-categories";
 	}
 
-	@RequestMapping("/category/{id}")
+	@GetMapping("/category/{id}")
 	public String findCategoryById(@PathVariable("id") Long id, Model model) {
 
 		model.addAttribute(ATTRIBUTE_NAME, categoryService.findCategoryById(id));
@@ -40,7 +41,7 @@ public class CategoryController {
 		return "add-category";
 	}
 
-	@RequestMapping("/add-category")
+	@PostMapping("/add-category")
 	public String createCategory(Category category, BindingResult result, Model model) {
 		if (result.hasErrors()) {
 			return "add-category";
@@ -58,7 +59,7 @@ public class CategoryController {
 		return "update-category";
 	}
 
-	@RequestMapping("/update-category/{id}")
+	@PostMapping("/update-category/{id}")
 	public String updateCategory(@PathVariable("id") Long id, Category category, BindingResult result, Model model) {
 		if (result.hasErrors()) {
 			category.setId(id);
@@ -70,7 +71,7 @@ public class CategoryController {
 		return "redirect:/categories";
 	}
 
-	@RequestMapping("/remove-category/{id}")
+	@GetMapping("/remove-category/{id}")
 	public String deleteCategory(@PathVariable("id") Long id, Model model) {
 		categoryService.deleteCategory(id);
 
