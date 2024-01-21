cd8cecc9a612b1379d168d6198ccfabc05315cbe
EsmaiLD <148991571+EsmaiLD@users.noreply.github.com>
2024-01-21 22:48:11 +0500

    mapper return obj direct and map func lambda defined

--- src/main/java/com/knf/dev/librarymanagementsystem/util/Mapper.java
+++ src/main/java/com/knf/dev/librarymanagementsystem/util/Mapper.java
@@ -19,37 +19,25 @@ public class Mapper {
 	}
 	public static List<BookRecord> bookModelToVo(List<Book> books) {
 
-		return books.stream().map(vo -> {
-			var bookVo = new BookRecord(vo.getId(), vo.getIsbn(), vo.getName(), vo.getSerialName(),
-					vo.getDescription());
-			return bookVo;
-		}).toList();
+		return books.stream().map(vo -> new BookRecord(vo.getId(), vo.getIsbn(), vo.getName(), vo.getSerialName(),
+				vo.getDescription())).toList();
 	}
 
 	public static List<AuthorRecord> authorModelToVo(List<Author> authors) {
 
-		return authors.stream().map(vo -> {
-			var authorVo = new AuthorRecord(vo.getId(), vo.getName(), vo.getDescription());
-			return authorVo;
-		}).toList();
+		return authors.stream().map(vo -> new AuthorRecord(vo.getId(), vo.getName(), vo.getDescription())).toList();
 
 	}
 
 	public static List<CategoryRecord> categoryModelToVo(List<Category> categories) {
 
-		return categories.stream().map(vo -> {
-			var categoryVo = new CategoryRecord(vo.getId(), vo.getName());
-			return categoryVo;
-		}).toList();
+		return categories.stream().map(vo -> new CategoryRecord(vo.getId(), vo.getName())).toList();
 
 	}
 
 	public static List<PublisherRecord> publisherModelToVo(List<Publisher> publishers) {
 
-		return publishers.stream().map(vo -> {
-			var publisherVo = new PublisherRecord(vo.getId(), vo.getName());
-			return publisherVo;
-		}).toList();
+		return publishers.stream().map(vo -> new PublisherRecord(vo.getId(), vo.getName())).toList();
 
 	}
 
